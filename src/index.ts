3f0359c0e7ff33884ed44d30ffe5b51ebe0a6290
Jazziel Puente <jazziel.puente.v@gmail.com>
2025-12-30 16:39:15 -0600

    Create server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import express, { type Request, type Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello via Bun and Express!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
+
 console.log("Hello via Bun!");
