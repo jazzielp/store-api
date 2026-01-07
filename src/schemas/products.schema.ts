00a6de52272e1f7d57a774043e73a9cd36a78b1b
Jazziel Puente <jazziel.puente.v@gmail.com>
2026-01-07 13:37:52 -0600

    feat: create product schema with zod

---
+++ src/schemas/products.schema.ts
@@ -0,0 +1,31 @@
+import { string, z } from "zod";
+
+export const createProdcutSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        message: "Name is required",
+      })
+      .min(1, "Name cannot be empty"),
+    price: z
+      .number({
+        message: "Price is required",
+      })
+      .nonnegative("Price cannot be negative"),
+  }),
+});
+
+export const updateProductSchema = z.object({
+  body: z.object({
+    name: z.string().min(1, "Name cannot be empty").optional(),
+    price: z.number().nonnegative("Price cannot be negative").optional(),
+  }),
+});
+
+export const getProductSchema = z.object({
+  params: z.object({
+    id: z.number({
+      message: "Id is required",
+    }),
+  }),
+});
