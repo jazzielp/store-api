52209b8169e10e8f18a1c29456c086dfde7408ff
Jazziel Puente <jazziel.puente.v@gmail.com>
2026-01-14 11:21:52 -0600

    feat: implement user routes with validation and controller integration

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,1 +1,22 @@
-import express, {type Router} from "express"
+import express, { type Router } from "express";
+import { validate } from "@/middlewares/validate";
+import {
+  createUserSchema,
+  getUserSchema,
+  updateUserSchema,
+} from "@/schemas/user.schema.ts";
+import * as UserController from "@/controllers/user.controller";
+
+const routes: Router = express.Router();
+routes.get("/", UserController.getAll);
+routes.get("/:id", validate(getUserSchema), UserController.getById);
+routes.post("/", validate(createUserSchema), UserController.create);
+routes.put(
+  "/:id",
+  validate(getUserSchema),
+  validate(updateUserSchema),
+  UserController.update
+);
+routes.delete("/:id", validate(getUserSchema), UserController.remove);
+
+export default routes;
