1c6c1549043fb80eaa8153df2a9bde5295d40eb9
Jazziel Puente <jazziel.puente.v@gmail.com>
2026-01-14 11:22:38 -0600

    feat: implement user management with CRUD operations and password hashing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express, { type Application } from "express";
 import productRoutes from "./products.route.ts";
+import userRoutes from "./user.route.ts";
 
 export function routerApi(app: Application): void {
   const router = express.Router();
   app.use("/api/v1", router);
 
   router.use("/products", productRoutes);
+  router.use("/users", userRoutes);
 }
